7d35a824b9cc955f40574417517f4d60fb2fef99
Ahmed K. A <ahmadkabdullah@pm.me>
2023-04-26 15:24:51 +0300

    Add basic answering and bot running

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,14 @@
-console.log("hello");
-console.log("hello");
+import { load } from "ts-dotenv";
+import { Telegraf } from "telegraf";
+
+const env = load({
+	BOT_TOKEN: String,
+});
+
+const bot = new Telegraf(env.BOT_TOKEN);
+
+bot.on("message", async (ctx) => {
+	await ctx.reply(`Hello!`);
+});
+
+bot.launch();
