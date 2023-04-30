c603bd76514bb4b1237cc89a1351a2712df77bdb
Ahmed K. A <ahmadkabdullah@pm.me>
2023-04-30 21:18:37 +0300

    Remove dist dir content

--- dist/utils.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getToday = void 0;
-function getToday() {
-    var d = new Date();
-    var dateString = "".concat(d.getDate(), "/").concat(d.getMonth() + 1, "/").concat(d.getFullYear());
-    return dateString;
-}
-exports.getToday = getToday;
