5a5ad8a5eaef85bfe9406de88a58f419c2f1864a
Ahmed K. A <ahmadkabdullah@pm.me>
2023-05-04 11:56:06 +0300

    Add new db connection with grammy plugins

--- src/env.ts
+++ src/env.ts
@@ -5,10 +5,6 @@ const schema = {
 	AI_KEY: String,
 	TRANSL_KEY: String,
 	DB_URL: String,
-	DB_NAME: String,
-	DB_PASS: String,
-	DB_USER: String,
-	DB_PORT: Number,
 };
 
 type Env = EnvType<typeof schema>;
--- src/main.ts
+++ src/main.ts
@@ -3,32 +3,46 @@ import { limitRequests, logRequests } from "./middleware";
 import { handleErrors, handleMessage, handleStart } from "./handlers";
 import { ContextExt } from "./types";
 import { getToday } from "./utils";
-import { Bot, session } from "grammy";
+import { Bot, Context, session } from "grammy";
+import { PsqlAdapter } from "@grammyjs/storage-psql";
+import { Client } from "pg";
 
 // load env variables
 loadEnv();
 
-// make the bot
-const bot = new Bot<ContextExt>(env.BOT_TOKEN);
+async function bootstrap() {
+	// connect the database
+	const client = new Client({
+		connectionString: env.DB_URL,
+	});
+	await client.connect();
 
-bot.use(
-	session({
-		initial: () => ({
-			dailyMessages: 4,
-			lastDate: getToday(),
-			messagesLeft: 4,
+	// make the bot
+	const bot = new Bot<ContextExt>(env.BOT_TOKEN);
+
+	bot.use(
+		session({
+			initial: () => ({
+				dailyMessages: 4,
+				lastDate: getToday(),
+				messagesLeft: 4,
+			}),
+			getSessionKey: (ctx: Context) => ctx.from?.id.toString(),
+			storage: await PsqlAdapter.create({ tableName: "sessions", client }),
 		}),
-	}),
-);
+	);
 
-bot.use(logRequests, limitRequests);
+	bot.use(logRequests, limitRequests);
 
-// add handlers
-bot.command("start", handleStart);
-bot.on("message", handleMessage);
-bot.catch(handleErrors);
+	// add handlers
+	bot.command("start", handleStart);
+	bot.on("message", handleMessage);
+	bot.catch(handleErrors);
 
-// launch the bot
-bot.start({
-	onStart: () => console.log("bot started!"),
-});
+	// launch the bot
+	bot.start({
+		onStart: () => console.log("bot started!"),
+	});
+}
+
+bootstrap();
